790eab2c5d6f6e901f465f0a3f820ebb5f442414
markpattison <mr.pattison@gmail.com>
2020-03-07 18:22:18 +0000

    tidy up F# project

--- IntrinsicsFSharp/Benchmarks.fs
+++ IntrinsicsFSharp/Benchmarks.fs
@@ -1,6 +1,10 @@
 ﻿module TryIntrinsics.Benchmarks
 
 open System
+open System.Runtime.Intrinsics
+open System.Runtime.Intrinsics.X86
+open System.Threading.Tasks
+
 open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
 
@@ -9,49 +13,229 @@ type SumProductComparison () =
     [<DefaultValue>] val mutable Data1 : float[]
     [<DefaultValue>] val mutable Data2 : float[]
 
-    [<Params(100000)>]
-    [<DefaultValue>] val mutable N : int
+    let n = 1000000
 
     [<GlobalSetup>]
     member this.InitialiseTestData() =
-        this.Data1 <- TestData.testData1 this.N
-        this.Data2 <- TestData.testData2 this.N
+        this.Data1 <- Array.init n (fun i -> 10.0 * sin (float i) + log (float i + 10.0))
+        this.Data2 <- Array.init n (fun i -> 15.0 * (0.5 + cos (0.25 * float i)))
 
     [<Benchmark>]
-    member this.Map2() = SumProduct.map2 this.Data1 this.Data2
+    member this.Map2() =
+        this.Data1
+        |> Array.map2 (*) this.Data2
+        |> Array.sum
 
     [<Benchmark>]
-    member this.AccumulateIter() = SumProduct.accumulateIter this.Data1 this.Data2
+    member this.AccumulateIter() =
+        let mutable acc = 0.0
+
+        this.Data1
+        |> Array.iteri (fun n x ->
+            acc <- acc + x * this.Data2.[n])
+        acc
 
     [<Benchmark>]
-    member this.AccumulateFor() = SumProduct.accumulateFor this.Data1 this.Data2
+    member this.AccumulateFor() =
+        let mutable acc = 0.0
+
+        for i in 0 .. (n - 1) do
+            acc <- acc + this.Data1.[i] * this.Data2.[i]
+        acc
 
     [<Benchmark>]
-    member this.NaiveParallelFor() = SumProduct.naiveParallelFor this.Data1 this.Data2
+    member this.NaiveParallelFor() =
+        let sums = Array.zeroCreate n
+
+        Parallel.For(0, n, fun i ->
+            sums.[i] <- this.Data1.[i] * this.Data2.[i]) |> ignore
+
+        Array.sum sums
 
     [<Benchmark>]
-    member this.MiddlingParallelFor() = SumProduct.middlingParallelFor this.Data1 this.Data2
+    member this.MiddlingParallelFor() =
+        let degrees = 4
+        let sums = Array.zeroCreate degrees
+
+        Parallel.For(0, degrees, fun p ->
+            let mutable acc = 0.0
+
+            for i in p .. degrees .. (n - 1)  do
+                acc <- acc + this.Data1.[i] * this.Data2.[i]
+    
+            sums.[p] <- acc) |> ignore
+
+        let mutable acc = 0.0
+        for i in 0 .. (sums.Length - 1) do
+            acc <- acc + sums.[i]
+        acc
 
     [<Benchmark>]
-    member this.SmartParallelFor() = SumProduct.smartParallelFor this.Data1 this.Data2
+    member this.SmartParallelFor() =
+        let degrees = 4
+        let sums = Array.zeroCreate degrees
+
+        Parallel.For(0, degrees, fun p ->
+            let mutable acc = 0.0
+            let startIndex = n * p / degrees
+            let endIndex =
+                if p = degrees - 1 then
+                    n - 1
+                else
+                    n * (p + 1) / degrees - 1
+
+            for i in startIndex .. endIndex do
+                acc <- acc + this.Data1.[i] * this.Data2.[i]
+    
+            sums.[p] <- acc) |> ignore
+
+        let mutable acc = 0.0
+        for i in 0 .. (sums.Length - 1) do
+            acc <- acc + sums.[i]
+        acc
 
     [<Benchmark>]
-    member this.Sse2() = SumProduct.withSse2 this.Data1 this.Data2
+    member this.Sse2() =
+        if not Sse2.IsSupported then failwith "SSE2 not available"
+
+        use d1 = fixed this.Data1
+        use d2 = fixed this.Data2
+
+        let mutable accV = Vector128<float>.Zero // holds two double-precision floats
+        let mutable mulV = Vector128<float>.Zero
+
+        let lastIndex = n - (n / 2) - 2
+
+        for i in 0 .. 2 .. lastIndex do
+            mulV <- Sse2.Multiply(Sse2.LoadVector128(NativeInterop.NativePtr.add d1 i), Sse2.LoadVector128(NativeInterop.NativePtr.add d2 i))
+            accV <- Sse2.Add(accV, mulV)
+
+        let mutable acc = accV.GetElement(0) + accV.GetElement(1)
+
+        // any remaining elements
+        for i in lastIndex + 1 .. n - 1 do
+            acc <- acc + this.Data1.[i] * this.Data2.[i]
+
+        acc
 
     [<Benchmark>]
-    member this.Sse2B() = SumProduct.withSse2B this.Data1 this.Data2
+    member this.Sse2B() =
+        if not Sse2.IsSupported then failwith "SSE2 not available"
+
+        use d1 = fixed this.Data1
+        use d2 = fixed this.Data2
+
+        let mutable accV = Vector128<float>.Zero // holds two double-precision floats
+        let mutable mulV = Vector128<float>.Zero
+
+        let lastIndex = n - (n / 2) - 2
+
+        for i in 0 .. 2 .. lastIndex do
+            mulV <- Sse2.Multiply(Sse2.LoadVector128(NativeInterop.NativePtr.add d1 i), Sse2.LoadVector128(NativeInterop.NativePtr.add d2 i))
+            accV <- Sse2.Add(accV, mulV)
+
+        let mutable acc = accV.GetElement(0) + accV.GetElement(1)
+
+        // any remaining elements
+        for i in lastIndex + 1 .. n - 1 do
+            acc <- acc + this.Data1.[i] * this.Data2.[i]
+
+        acc
 
     [<Benchmark>]
-    member this.Sse3() = SumProduct.withSse3 this.Data1 this.Data2
+    member this.Sse3() =
+        if not Sse3.IsSupported then failwith "SSE3 not available"
+
+        use d1 = fixed this.Data1
+        use d2 = fixed this.Data2
+
+        let mutable accV = Vector128<float>.Zero // holds two double-precision floats
+        let mutable mulV = Vector128<float>.Zero
+
+        let lastIndex = n - (n / 2) - 2
+
+        for i in 0 .. 2 .. lastIndex do
+            mulV <- Sse3.Multiply(Sse3.LoadVector128(NativeInterop.NativePtr.add d1 i), Sse3.LoadVector128(NativeInterop.NativePtr.add d2 i))
+            accV <- Sse3.Add(accV, mulV)
+
+        let mutable acc = accV.GetElement(0) + accV.GetElement(1)
+
+        // any remaining elements
+        for i in lastIndex + 1 .. n - 1 do
+            acc <- acc + this.Data1.[i] * this.Data2.[i]
+
+        acc
 
     [<Benchmark>]
-    member this.Avx() = SumProduct.withAvx this.Data1 this.Data2
+    member this.Avx() =
+        if not Avx.IsSupported then failwith "AVX not available"
+
+        use d1 = fixed this.Data1
+        use d2 = fixed this.Data2
+
+        let mutable accV = Vector256<float>.Zero // holds four double-precision floats
+        let mutable mulV = Vector256<float>.Zero
+
+        let lastIndex = n - (n / 4) - 4
+
+        for i in 0 .. 4 .. lastIndex do
+            mulV <- Avx.Multiply(Avx.LoadVector256(NativeInterop.NativePtr.add d1 i), Avx.LoadVector256(NativeInterop.NativePtr.add d2 i))
+            accV <- Avx.Add(accV, mulV)
+
+        let mutable acc = accV.GetElement(0) + accV.GetElement(1) + accV.GetElement(2) + accV.GetElement(3)
+
+        // any remaining elements
+        for i in lastIndex + 1 .. n - 1 do
+            acc <- acc + this.Data1.[i] * this.Data2.[i]
+
+        acc
 
     [<Benchmark>]
-    member this.Avx2() = SumProduct.withAvx2 this.Data1 this.Data2
+    member this.Avx2() =
+        if not Avx2.IsSupported then failwith "AVX2 not available"
+
+        use d1 = fixed this.Data1
+        use d2 = fixed this.Data2
+
+        let mutable accV = Vector256<float>.Zero // holds four double-precision floats
+        let mutable mulV = Vector256<float>.Zero
+
+        let lastIndex = n - (n / 4) - 4
+
+        for i in 0 .. 4 .. lastIndex do
+            mulV <- Avx2.Multiply(Avx2.LoadVector256(NativeInterop.NativePtr.add d1 i), Avx2.LoadVector256(NativeInterop.NativePtr.add d2 i))
+            accV <- Avx2.Add(accV, mulV)
+
+        let mutable acc = accV.GetElement(0) + accV.GetElement(1) + accV.GetElement(2) + accV.GetElement(3)
+
+        // any remaining elements
+        for i in lastIndex + 1 .. n - 1 do
+            acc <- acc + this.Data1.[i] * this.Data2.[i]
+
+        acc
 
     [<Benchmark>]
-    member this.Fma() = SumProduct.withFma this.Data1 this.Data2
+    member this.Fma() =
+        if not Fma.IsSupported then failwith "FMA not available"
+
+        use d1 = fixed this.Data1
+        use d2 = fixed this.Data2
+
+        let mutable accV = Vector256<float>.Zero // holds four double-precision floats
+        let mutable mulV = Vector256<float>.Zero
+
+        let lastIndex = n - (n / 4) - 4
+
+        for i in 0 .. 4 .. lastIndex do
+            accV <- Fma.MultiplyAdd(accV, Fma.LoadVector256(NativeInterop.NativePtr.add d1 i), Fma.LoadVector256(NativeInterop.NativePtr.add d2 i))
+
+        let mutable acc = accV.GetElement(0) + accV.GetElement(1) + accV.GetElement(2) + accV.GetElement(3)
+
+        // any remaining elements
+        for i in lastIndex + 1 .. n - 1 do
+            acc <- acc + this.Data1.[i] * this.Data2.[i]
+
+        acc
 
 [<EntryPoint>]
 let Main _ =
--- IntrinsicsFSharp/TestData.fs
+++
@@ -1,8 +0,0 @@
-﻿module TryIntrinsics.TestData
-
-let testData1 n =
-    Array.init n (fun i -> 10.0 * sin (float i) )
-
-let testData2 n =
-    Array.init n (fun i -> 15.0 * (0.5 + cos (0.25 * float i)))
-
